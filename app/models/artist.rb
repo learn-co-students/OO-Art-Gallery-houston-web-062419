f56da4abbb61b6a054ac25a3078872d9c6d6da6b
jordanbacon <jmbacon92@gmail.com>
2019-06-28 16:48:51 -0700

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,50 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    self.paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    self.galleries.map do |gallery|
+      gallery.city
+    end.uniq
+  end
+
+  def self.total_experience
+    all.sum do |num|
+      num.years_experience
+    end
+  end
+
+  def self.most_prolific
+    all.max do |a1, a2| 
+    a1.paintings.size / a1.years_experience <=> a2.paintings.size / a2.years_experience
+    end
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(self, title, price, gallery)
   end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,38 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+  end
+
+  def artists
+    paintings.map do |p|
+      p.artist
+    end
+  end
+
+  def artist_names
+    self.paintings.map do |name|
+      name.gallery
+    end
+  end
+
+  def most_expensive_painting
+    paintings.max {|p1, p2| p1.price <=> p2.price}
   end
 
 end
