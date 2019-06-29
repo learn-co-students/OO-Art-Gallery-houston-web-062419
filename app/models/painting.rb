3a754c16c7b9eac5e22284c26f8df0fb7b13440e
github username <github email address>
2019-06-29 00:29:02 -0500

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,27 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_accessor :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(artist, gallery, title, price)
+    @artist = artist
+    @gallery = gallery
     @title = title
     @price = price
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price #Returns an integer that is the total price of all paintings
+    self.all.sum do |k|
+      k.price
+    end
+  end
+
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,19 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+
+a1 = Artist.new("Hanky Sparts", 6)
+a2 = Artist.new("Janique Cole", 3)
+
+g1 = Gallery.new("MOMA", "New York")
+g2 = Gallery.new("MOMO", "Venice")
+
+p1 = Painting.new(a1, g1, "Green Meadows", 1200)
+p2 = Painting.new(a2, g2, "Blue Sparrows", 1000)
+p3 = Painting.new(a1, g2, "Hunter Meadows", 1300)
+p4 = Painting.new(a2, g1, "Green Sparrows", 1100)
+p5 = Painting.new(a1, g1, "Blue Meadows", 1220)
+
+binding.pry
+0
